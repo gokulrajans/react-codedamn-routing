57d86312ae320c7c8451b8c9b5ce3e969f2fa116
GOKULRAJ <gokul19.v@gmail.com>
2022-05-03 06:02:53 +0530

    Committing old changes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/hellow-world">
+          <h1> This is Hellow World Page.... </h1>
+        </Route>
+        </Routes>
+      </BrowserRouter>
+      <h1> This is Normal Home Page </h1>
+    </div></>
   );
 }
 
